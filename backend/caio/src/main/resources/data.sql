dc943ff4b14e00be7156f74eca0fb469875df1e5
Andesson Reis <andessonreys@gmail.com>
2025-01-06 21:44:46 -0300

    chore: add supabase

--- backend/caio/src/main/resources/data.sql
+++ backend/caio/src/main/resources/data.sql
@@ -1,4 +1,8 @@
+-- Cria a extensão pgcrypto, se não existir
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- Inserir roles
-INSERT INTO roles (id, enum_roles) VALUES
-    (UUID(), 'ROLE_USER'),  -- ROLE_USER 
-    (UUID(), 'ROLE_ADMIN');  -- ROLE_ADMIN 
+INSERT INTO roles (id, enum_roles)
+VALUES 
+  (gen_random_uuid(), 'ROLE_USER'), 
+  (gen_random_uuid(), 'ROLE_ADMIN');
